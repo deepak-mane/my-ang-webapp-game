367fd95ba39eb76f6e6877c5a148f4f83f3b2c2e
deepak-mane <deepak.mane123@gmail.com>
2019-06-03 17:53:17 -0700

    first updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameContorlComponent } from './game-contorl/game-contorl.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameContorlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/game-contorl/game-contorl.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-contorl',
+  templateUrl: './game-contorl.component.html',
+  styleUrls: ['./game-contorl.component.css']
+})
+export class GameContorlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
